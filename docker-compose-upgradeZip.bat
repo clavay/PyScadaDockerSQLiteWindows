e54d743d21dcb1a6ebddc6eb4a846d4412c9dbab
clavay <clavayssiere@univ-pau.fr>
2026-02-10 18:41:26 +0100

    update projet template settings

--- docker-compose-upgradeZip.bat
+++ docker-compose-upgradeZip.bat
@@ -1,4 +1,4 @@
 docker container exec pyscada /bin/sh -c "for file in `ls -1 /src/pyscada/shared/plugins/*.zip`; do pip3 install $file; done"
 docker container exec pyscada /bin/sh -c "python3 /src/pyscada/manage.py migrate; python3 /src/pyscada/manage.py collectstatic --no-input"
-docker container restart pyscada nginx
+docker container restart pyscada nginx gunicorn
 pause
